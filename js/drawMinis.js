38c9903142abf225011325988adee44738993ce6
Roman Milaev <r.milaev@yandex.ru>
2023-10-30 01:36:12 +0300

    Изменил место для отображения в разметке фотографий вместо шаблона

--- js/drawMinis.js
+++ js/drawMinis.js
@@ -1,6 +1,6 @@
 import {createSimilarPhotoDescriptions} from './data.js';
 
-const MINI_PICTURE_PLACE = document.querySelector('#picture');
+const MINI_PICTURE_PLACE = document.querySelector('.pictures');
 const MINI_PICTURE_TEMPLATE = document.querySelector('#picture').content.querySelector('.picture');
 const SIMILAR_PHOTO_POSTS = createSimilarPhotoDescriptions();
 const SIMILAR_POST_FRAGMENT = document.createDocumentFragment();

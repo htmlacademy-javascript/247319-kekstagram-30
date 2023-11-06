9b67d8f6742edff99250be3f0043b0fd11d54eea
Roman Milaev <r.milaev@yandex.ru>
2023-11-06 23:26:24 +0300

    Комментариий загружаются по 5 штук

--- js/big-photo-post.js
+++ js/big-photo-post.js
@@ -4,17 +4,26 @@ const photoDescription = document.querySelector('.social__caption');
 const commentsList = document.querySelector('.social__comments');
 const commentsCount = document.querySelector('.social__comment-shown-count');
 const totalCommentsCount = document.querySelector('.social__comment-total-count');
+const commentsLoader = document.querySelector('.comments-loader');
+const COMMENTS_PER_PAGE = 5;
+let commentsShownCount = COMMENTS_PER_PAGE;
+let comments = [];
 
 function createBigPhotoPost(photoData) {
   bigPictureImg.src = photoData.url;
   bigPictureImg.alt = photoData.description;
   photoDescription.textContent = photoData.description;
   likesCount.textContent = photoData.likes;
-  commentsCount.textContent = photoData.comments.length;
-  totalCommentsCount.textContent = photoData.comments.length;
+  comments = photoData.comments;
+  commentsShownCount = COMMENTS_PER_PAGE;
+  createCommentsForBigPhotoPost();
+}
 
+function createCommentsForBigPhotoPost() {
   commentsList.innerHTML = '';
-  photoData.comments.forEach((comment) => {
+  const commentsToShow = comments.slice(0, commentsShownCount);
+
+  commentsToShow.forEach((comment) => {
     const commentItem = document.createElement('li');
     commentItem.classList.add('social__comment');
 
@@ -33,6 +42,25 @@ function createBigPhotoPost(photoData) {
     commentItem.appendChild(commentText);
     commentsList.appendChild(commentItem);
   });
+
+  if (commentsShownCount >= comments.length) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  commentsCount.textContent = Math.min(commentsShownCount, comments.length);
+  totalCommentsCount.textContent = comments.length;
 }
+
+function onloadMoreComments () {
+  commentsShownCount += COMMENTS_PER_PAGE;
+  if (commentsShownCount > comments.length) {
+    commentsShownCount = comments.length;
+  }
+  createCommentsForBigPhotoPost();
+}
+
+commentsLoader.addEventListener('click', onloadMoreComments);
 
 export {createBigPhotoPost};
--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,8 @@
 import {isEscapeKey} from './utils.js';
 
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureOpen = document.querySelector('.pictures');
 const bigPictureClose = document.querySelector('.big-picture__cancel');
 const body = document.querySelector('body');
-const commentsBlock = document.querySelector('.social__comment-count');
-const commentsLoaderButton = document.querySelector('.comments-loader');
 
 function onDocumentKeydown(evt) {
   if (isEscapeKey(evt)) {
@@ -17,8 +14,6 @@ function onDocumentKeydown(evt) {
 function openBigPicture () {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentsBlock.classList.add('hidden');
-  commentsLoaderButton.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 }
 
@@ -27,10 +22,6 @@ function closeBigPicture () {
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 }
-
-bigPictureOpen.addEventListener('click', () => {
-  openBigPicture();
-});
 
 bigPictureClose.addEventListener('click', () => {
   closeBigPicture();

ae70bd040457554e32137061ab59ad4c16d119c2
Roman Milaev <r.milaev@yandex.ru>
2023-11-17 18:31:53 +0300

    Убрал дестрой слайдера пока что, все работает

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 import {isEscapeKey} from './utils.js';
-import {changeEffects, destroySlider} from './effects-editor.js';
+import {changeEffects} from './effects-editor.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const imgUpload = document.querySelector('.img-upload__overlay');
@@ -56,7 +56,6 @@ imgUploadPlace.addEventListener('change', () => {
 
 imgUploadClose.addEventListener('click', () => {
   closeUploadImgForm();
-  destroySlider();
 });
 
 function validateHashtags () {
